3f02e6ebea623d9ca7b7fc3a250534d079afaa40
ppadjin <pavle.padjin@gmail.com>
2025-05-09 10:48:21 +0100

    met3r loss wrapper and pose sampling module

---
+++ src/met3r_loss.py
@@ -0,0 +1,248 @@
+import torch
+from met3r.met3r.met3r import MEt3R
+from torchvision import transforms
+from torch.nn import functional as F
+
+class Met3rLoss(torch.nn.Module):
+    
+    DEVICE = 'cuda'        
+    backbone_to_patch_size = {
+        'dino16': 32,
+        'dinov2': 14
+    }
+    showed_warning_padding = False
+    
+    def __init__(self, feat_backbone: str = 'dino16'):
+        super().__init__()
+
+        self.orig_size = None
+        self.feat_backbone = feat_backbone
+        self.patch_size = self.backbone_to_patch_size[feat_backbone]
+        
+        # by default, we don't pad the image
+        self.pad = False
+        self.metric = MEt3R(
+            img_size=None,
+            use_norm=True,
+            feat_backbone=feat_backbone,
+            patch_size=self.patch_size,
+            featup_weights='mhamilton723/FeatUp',
+            dust3r_weights='naver/MASt3R_ViTLarge_BaseDecoder_512_catmlpdpt_metric',
+            use_mast3r_dust3r=True
+        ).cuda()
+
+    def __set_scaling_factor(self, image_size: tuple[int, int]) -> float:
+        """
+        Get the scaling factor for the image size. This is important to make sure
+        the resulting image size divisible by 16.
+        """
+        if image_size[0] == 540 and image_size[1] == 960:
+            self.scaling_factor = 8 / 15
+        
+        elif image_size[0] == 720 and image_size[1] == 960:
+            self.scaling_factor = 2/3
+        
+        elif image_size[0] == 1706 and image_size[1] == 960: # iphone 14 pro
+            self.scaling_factor = 1/3
+        # in future if needed, provide more scaling factor for different sizes
+        else:
+            self.scaling_factor = 1.0
+
+        if image_size[0] * self.scaling_factor % self.patch_size != 0 or image_size[1] * self.scaling_factor % self.patch_size != 0:
+            if not self.showed_warning_padding:
+                print(f"Warning: The image size {image_size} is not divisible by {self.patch_size}, padding will be applied")
+                self.showed_warning_padding = True
+            self.pad = True
+            
+    def preprocess_tensor(self, image: torch.Tensor, resize: bool = True) -> torch.Tensor:
+        """
+        Preprocess a tensor for MEt3R.
+        Normalize to [-1,1], centercrop + resize to result_size x result_size
+        """
+        if image.shape[-1] in {1, 3}:  # a.k.a. if in format (H, W, C)
+            image = image.permute(2, 0, 1)
+
+        image = ((image - image.min()) / (image.max() - image.min()) - 0.5) * 2
+        image = image.cuda()
+        
+        if resize:
+            result_size = (int(image.shape[1] * self.scaling_factor), int(image.shape[2] * self.scaling_factor))
+            image = transforms.Resize(result_size)(image)
+        
+        return image
+
+    def __transform_calibration_matrix(self, K: torch.Tensor) -> torch.Tensor:
+        K_prim = torch.tensor([
+            [self.scaling_factor * K[0, 0], 0, self.scaling_factor * K[0, 2]],
+            [0, self.scaling_factor * K[1, 1], self.scaling_factor * K[1, 2]],
+            [0, 0, 1]
+        ]).to(Met3rLoss.DEVICE)
+        return K_prim
+
+    def preprocess_depth(self, depth: torch.Tensor, resize: bool = True) -> torch.Tensor:
+        """Preprocess a depth tensor for MEt3R.
+        Normalize to [0,1], pad to the largest dimension of the image"""
+        depth = depth.squeeze().unsqueeze(0)
+        assert depth.dim() == 3
+        depth = depth.cuda()
+        if resize:
+            result_size = (int(depth.shape[1] * self.scaling_factor), int(depth.shape[2] * self.scaling_factor))
+            depth = transforms.Resize(result_size)(depth)
+        return depth
+
+    def forward(
+        self,
+        gt_image: torch.Tensor,
+        ood_rendered_image: torch.Tensor,
+        gt_depth: torch.Tensor,
+        ood_depth: torch.Tensor,
+        gt_pose: torch.Tensor,
+        ood_pose: torch.Tensor,
+        calibration_matrix: torch.Tensor,
+        valid_mask: torch.Tensor | None = None,
+        use_oclusion_mask: bool = True,
+        **kwargs):
+        """
+        Compute the MET3R loss between a ground truth image and an OOD rendered image.
+        The loss is computed only on the valid pixels.
+        The weight_map is used to weight the loss of the OOD pixels.
+        The weight_map is a tensor of the same size as the ground truth image.
+        The weight_map is a tensor of the same size as the ground truth image.
+
+        Args:
+            gt_image (torch.Tensor): Ground truth image tensor of shape (H, W, 3)
+            ood_rendered_image (torch.Tensor): Out-of-distribution rendered image tensor of shape (H, W, 3)
+            gt_depth (torch.Tensor): Ground truth depth tensor of shape (H, W, 1)
+            ood_depth (torch.Tensor): Out-of-distribution depth tensor of shape (H, W, 1)
+            gt_pose (torch.Tensor): Ground truth pose tensor of shape (4, 4)
+            ood_pose (torch.Tensor): Out-of-distribution pose tensor of shape (4, 4)
+            calibration_matrix (torch.Tensor): Calibration matrix tensor of shape (3, 3)
+            valid_mask (torch.Tensor, optional): Binary mask indicating valid pixels of shape (H, W, 1). Defaults to None.
+            weight_map (torch.Tensor, optional): Weight map for loss computation of shape (H, W, 1). Defaults to None.
+
+        Returns:
+            score: MET3R score
+            score_map: MET3R score map
+        """
+        self.orig_size = gt_image.shape[-3:-1]  # assumes (H, W, C)
+        assert gt_image.shape == ood_rendered_image.shape
+        assert gt_pose.shape == ood_pose.shape == (4, 4)
+        assert calibration_matrix.shape == (3, 3)
+        assert gt_image.dim() == 3
+        self.__set_scaling_factor(self.orig_size)
+
+        gt_image = self.preprocess_tensor(gt_image)
+        ood_rendered_image = self.preprocess_tensor(ood_rendered_image)
+        calibration_matrix = self.__transform_calibration_matrix(calibration_matrix)
+        gt_depth = self.preprocess_depth(gt_depth)
+        ood_depth = self.preprocess_depth(ood_depth)
+
+        _, overlap_mask, score_map = self.metric(
+            train_rgb=gt_image,
+            ood_rgb=ood_rendered_image,
+            train_depth=gt_depth,
+            ood_depth=ood_depth,
+            train_pose=gt_pose,
+            ood_pose=ood_pose,
+            camera_matrix=calibration_matrix,
+            return_overlap_mask=True,
+            return_score_map=True,
+            return_projections=False,
+            use_oclusion_mask=use_oclusion_mask,
+            pad=self.pad,
+            **kwargs)
+
+        score_map = score_map * overlap_mask
+
+        score_map[score_map == 1.0] = 0.0
+
+        if max(self.orig_size) > max(score_map.shape):
+            score_map = F.interpolate(score_map.unsqueeze(0), size=self.orig_size, mode='bilinear', align_corners=False).squeeze()
+
+        if valid_mask is not None:
+            score_map = score_map * valid_mask.squeeze().to(Met3rLoss.DEVICE)
+
+        score = score_map[score_map != 0].mean()
+
+        score_map = score_map.detach().cpu()
+        return score, score_map.squeeze().unsqueeze(-1), overlap_mask.detach().cpu().squeeze().unsqueeze(-1)
+
+    def forward_multiview_consistency(
+        self,
+        gt_image: torch.Tensor,
+        ood_rendered_image: torch.Tensor,
+        gt_depth: torch.Tensor,
+        ood_depth: torch.Tensor,
+        gt_pose: torch.Tensor,
+        ood_pose: torch.Tensor,
+        calibration_matrix: torch.Tensor,
+        valid_mask: torch.Tensor | None = None,
+        use_oclusion_mask: bool = True,
+        **kwargs):
+
+        assert gt_image.shape == ood_rendered_image.shape
+        assert gt_pose.shape == ood_pose.shape == (4, 4)
+        assert calibration_matrix.shape == (3, 3)
+        assert gt_image.dim() == 3
+
+        gt_image = self.preprocess_tensor(gt_image, resize=False)
+        ood_rendered_image = self.preprocess_tensor(ood_rendered_image, resize=False)
+        gt_depth = self.preprocess_depth(gt_depth, resize=False)
+        ood_depth = self.preprocess_depth(ood_depth, resize=False)
+
+        l1_loss_map, overlap_mask, projections = self.metric.forward_rgb_features(
+            train_rgb=gt_image,
+            ood_rgb=ood_rendered_image,
+            train_depth=gt_depth,
+            ood_depth=ood_depth,
+            train_pose=gt_pose,
+            ood_pose=ood_pose,
+            camera_matrix=calibration_matrix,
+            return_overlap_mask=True,
+            return_projections=True,
+            use_oclusion_mask=use_oclusion_mask,
+            **kwargs)
+        
+        _, gt_projections = torch.unbind(projections, dim=1)
+        if valid_mask is not None:
+            overlap_mask = overlap_mask * valid_mask.squeeze().to(Met3rLoss.DEVICE)
+            
+            
+        l1_loss = l1_loss_map[overlap_mask == 1.0].sum() / (overlap_mask == 1.0).sum().clamp(min=1e-3)
+        l1_loss_map = l1_loss_map.squeeze().mean(dim=-1).detach().cpu().unsqueeze(-1)
+        overlap_mask = overlap_mask.detach().cpu().squeeze().unsqueeze(-1)
+        gt_projections = gt_projections.squeeze().detach().cpu()
+        return l1_loss, l1_loss_map, gt_projections, overlap_mask
+    
+    def forward_depth_mvc(self,
+        gt_depth: torch.Tensor,
+        ood_depth: torch.Tensor,
+        gt_pose: torch.Tensor,
+        ood_pose: torch.Tensor,
+        calibration_matrix: torch.Tensor,
+        valid_mask: torch.Tensor | None = None,
+        use_oclusion_mask: bool = True,
+        **kwargs):
+        
+        gt_depth = self.preprocess_depth(gt_depth, resize=False)
+        ood_depth = self.preprocess_depth(ood_depth, resize=False)
+        
+        l1_loss_map, overlap_mask = self.metric.forward_depth_mvc(
+            train_depth=gt_depth,
+            ood_depth=ood_depth,
+            camera_matrix=calibration_matrix,
+            train_pose=gt_pose,
+            ood_pose=ood_pose,
+            return_overlap_mask=True,
+            use_oclusion_mask=use_oclusion_mask,
+            **kwargs)
+        
+        if valid_mask is not None:
+            overlap_mask = overlap_mask * valid_mask.squeeze().to(Met3rLoss.DEVICE)
+        
+        l1_loss = l1_loss_map[overlap_mask == 1.0].sum() / (overlap_mask == 1.0).sum().clamp(min=1e-3)
+        
+        l1_loss_map = l1_loss_map.squeeze().detach().cpu().unsqueeze(-1)
+        overlap_mask = overlap_mask.detach().cpu().squeeze().unsqueeze(-1)
+        return l1_loss, l1_loss_map, overlap_mask
+
---
+++ src/pose_sampler.py
@@ -0,0 +1,292 @@
+import math
+import torch
+import numpy as np
+
+
+class PoseSamplingBuilder:
+    """This class is a collection of functions that sample a new pose from a given pose."""
+    DEVICE = 'cuda'
+    T_SIGMA = 15.0
+
+    @staticmethod
+    def sample_vector_near(stddev):
+        """
+        Samples a vector near [0,0,-1] with norm 1, where the distance from [0,0,-1] follows a Gaussian distribution.
+
+        Args:
+            stddev (float): Standard deviation of the Gaussian distribution for the distance.
+
+        Returns:
+            torch.Tensor: The sampled vector with norm 1.
+        """
+        v = torch.tensor([0.0, 0.0, -1.0], device=PoseSamplingBuilder.DEVICE)
+        random_vector = torch.randn_like(v, device=PoseSamplingBuilder.DEVICE)
+
+        # vector ortogonal
+        orthogonal_vector = random_vector - torch.dot(random_vector, v) * v
+        orthogonal_vector = orthogonal_vector / torch.norm(orthogonal_vector)
+
+        distance = torch.randn(1, device=PoseSamplingBuilder.DEVICE) * stddev
+
+        new_vector = (v + distance * orthogonal_vector).float()
+        new_vector = new_vector / torch.norm(new_vector)
+        # upper hemisphere
+        new_vector[1] = torch.abs(new_vector[1])
+        return new_vector
+
+    @staticmethod
+    def normalize(v):
+        norm = torch.norm(v, dim=-1, keepdim=True)
+        return v / norm
+
+    @staticmethod
+    def create_rotation_matrix(z_cam: torch.Tensor):
+        # Reference up vector (world y-axis)
+        v_ref = torch.tensor([0.0, 1.0, 0.0], device=PoseSamplingBuilder.DEVICE)
+        
+        # Check if z_cam is too aligned with v_ref
+        dot_product = torch.dot(z_cam, v_ref)
+        epsilon = 1e-6
+        if torch.abs(dot_product) < 1 - epsilon:
+            x_cam = PoseSamplingBuilder.normalize(torch.cross(v_ref, z_cam, dim=-1))
+        else:
+            # Use x-axis as fallback
+            v_ref_alt = torch.tensor([1.0, 0.0, 0.0], device=PoseSamplingBuilder.DEVICE)
+            x_cam = PoseSamplingBuilder.normalize(torch.cross(v_ref_alt, z_cam, dim=-1))
+        
+        y_cam = PoseSamplingBuilder.normalize(torch.cross(z_cam, x_cam, dim=-1))
+        R = torch.stack([x_cam, y_cam, z_cam], dim=1).T
+        return R
+
+    @staticmethod
+    def spherical_pose_from_eye(look_at_point: torch.Tensor, stddev: float):
+        """
+        Samples a new pose similar to the identity pose, where it is sampled from a sphere centered at the look_at_point
+        and the radius is distance from the original pose to the look_at_point. The distance from the original pose
+        follows a Gaussian distribution with standard deviation stddev. Each sampled pose is still looking 
+        directly at the look_at_point (meaning that look_at_point is contained on the optical axis of the new pose camera).
+        """
+        pose = torch.eye(4, device=PoseSamplingBuilder.DEVICE)
+        look_at_point = look_at_point.float()
+        
+        look_at_cam = pose @ torch.cat([look_at_point, torch.tensor([1.0], device=PoseSamplingBuilder.DEVICE)])
+        d = look_at_cam[2]
+        
+        u = PoseSamplingBuilder.sample_vector_near(stddev)
+        C_new = look_at_point + d * u
+        
+        z_cam = PoseSamplingBuilder.normalize(look_at_point - C_new)  # Direction from C_new to look_at_point
+        R_new = PoseSamplingBuilder.create_rotation_matrix(z_cam)
+
+        T_new = -R_new @ C_new
+        
+        M_new = torch.eye(4, device=PoseSamplingBuilder.DEVICE)
+        M_new[:3, :3] = R_new
+        M_new[:3, 3] = T_new
+        return M_new
+    
+    @staticmethod
+    def get_rotation_matrix(theta_rad: float, axis: str) -> torch.Tensor:
+        if axis == 'x':
+            return torch.tensor([
+                [1, 0, 0],
+                [0, math.cos(theta_rad), -math.sin(theta_rad)],
+                [0, math.sin(theta_rad), math.cos(theta_rad)]
+            ], device=PoseSamplingBuilder.DEVICE)
+        elif axis == 'y':
+            return torch.tensor([
+                [math.cos(theta_rad), 0, math.sin(theta_rad)],
+                [0, 1, 0],
+                [-math.sin(theta_rad), 0, math.cos(theta_rad)]
+            ], device=PoseSamplingBuilder.DEVICE)
+        elif axis == 'z':
+            return torch.tensor([
+                [math.cos(theta_rad), -math.sin(theta_rad), 0],
+                [math.sin(theta_rad), math.cos(theta_rad), 0],
+                [0, 0, 1]
+            ], device=PoseSamplingBuilder.DEVICE)
+        else:
+            raise ValueError(f'Invalid axis: {axis}')
+    
+    @staticmethod
+    def rotate_pose_around_axis(pose: torch.Tensor, theta: float, axis: str) -> torch.Tensor:
+        theta_rad = theta * np.pi / 180
+        pose_inv = torch.inverse(pose)
+
+        R = PoseSamplingBuilder.get_rotation_matrix(theta_rad, axis)
+        rotation_matrix = R @ pose_inv[:3, :3]
+        pose_inv[:3, :3] = rotation_matrix
+
+        pose = torch.inverse(pose_inv)
+        return pose
+    
+    @staticmethod
+    def translate_pose(pose: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        pose_inv = torch.inverse(pose)
+        t_hom = torch.cat([t, torch.ones(1, device=pose.device)])
+        t_hom_wcf = pose_inv @ t_hom
+        t_wcf = t_hom_wcf[:3]
+        new_pose_inv = pose_inv.clone()
+        new_pose_inv[:3, 3] = t_wcf
+        new_pose = torch.inverse(new_pose_inv)
+        return new_pose
+    
+    @staticmethod
+    def translate_along_circle(pose: torch.Tensor, depth_map: torch.Tensor, K: torch.Tensor) -> torch.Tensor:
+        """
+        This implements the sampling of translation vector from https://arxiv.org/pdf/2502.07615.
+        The idea is to first calculate the radius with respect to the mean depth, hyperparameter T_SIGMA
+        and the focal length of the camera.
+        """
+        mean_focal_length = (K[0, 0] + K[1, 1]) / 2
+        radius = PoseSamplingBuilder.T_SIGMA * torch.mean(depth_map) / mean_focal_length
+        theta = torch.rand(1, device=PoseSamplingBuilder.DEVICE) * 2 * np.pi
+        translation = torch.tensor([
+            radius * torch.sin(theta),
+            radius * torch.cos(theta),
+            0
+        ], device=PoseSamplingBuilder.DEVICE)
+        new_pose = PoseSamplingBuilder.translate_pose(pose, translation)
+        return new_pose
+    
+    @staticmethod
+    def height_correction(pose: torch.Tensor, ref_pose: torch.Tensor) -> torch.Tensor:
+        """
+        This function corrects the height of the pose with respect to the reference pose.
+        """
+        pose_inv = torch.inverse(pose)
+        ref_pose_inv = torch.inverse(ref_pose)
+        pose_inv[2, 3] = ref_pose_inv[2, 3]
+        return torch.inverse(pose_inv)
+    
+    @staticmethod
+    def spherical_pose_augmentation(pose, sphere_radius: float, stddev: float):
+        """
+        Samples a new pose similar to the pose, where it is sampled from a sphere centered at the look_at_point
+        and the radius is distance from the original pose to the look_at_point. The distance from the original pose
+        follows a Gaussian distribution with standard deviation stddev. Each sampled pose is still looking 
+        directly at the look_at_point (meaning that look_at_point is contained on the optical axis of the new pose camera).
+        """
+        pose = pose.to(PoseSamplingBuilder.DEVICE)
+        look_at_point = torch.tensor([0.0, 0.0, sphere_radius], device=PoseSamplingBuilder.DEVICE)
+        sampled_pose_ccf = PoseSamplingBuilder.spherical_pose_from_eye(look_at_point, stddev)
+        sampled_pose_wcf = torch.inverse(pose) @ sampled_pose_ccf
+        sampled_pose_wcf = torch.inverse(sampled_pose_wcf)
+        return sampled_pose_wcf
+
+
+class PoseSampler:
+    DEVICE = 'cuda'
+    
+    def __init__(self, strategy: str, theta: float = None, sphere_radius: float = None, stddev: float = None):
+        self.theta = theta
+        self.sphere_radius = sphere_radius
+        assert stddev > 0, f'stddev must be greater than 0, got {stddev}'
+        self.stddev = stddev
+        self.strategy = strategy
+
+    def ood_translation_only(self, pose: torch.Tensor, depth_map: torch.Tensor, K: torch.Tensor, **kwargs):
+        """
+        Similar to sampling from flow distillation paper (https://arxiv.org/pdf/2502.07615.pdf).
+        Takes in the pose, performs downward rotation by theta, and then translates along
+        the circle with radius T_SIGMA * mean_depth / mean_focal_length.
+        """
+        pose = pose.to(PoseSampler.DEVICE)
+        pose = PoseSamplingBuilder.rotate_pose_around_axis(pose, self.theta, 'x')
+        pose = PoseSamplingBuilder.translate_along_circle(pose, depth_map, K)
+        return pose
+    
+    def ood_spherical_augmentation(self, pose: torch.Tensor, **kwargs):
+        """
+        Performs the downward rotation by theta, and then samples a new pose from a sphere centered at the look_at_point.
+
+        """
+        pose = pose.to(PoseSampler.DEVICE)
+        pose = PoseSamplingBuilder.rotate_pose_around_axis(pose, self.theta, 'x')
+        pose = PoseSamplingBuilder.spherical_pose_augmentation(pose, self.sphere_radius, self.stddev)
+        return pose
+    
+    def ood_spherical_auto(self, pose: torch.Tensor, depth_map: torch.Tensor, auto_height_correction: bool = True, **kwargs):
+        """
+        Performs translation forward followed by spherical augmentation. Estimates optimal parameters from depth map.
+        
+        Args:
+            pose (torch.Tensor): The pose to augment.
+            depth_map (torch.Tensor): The depth map from the pose.
+
+        Returns:
+            torch.Tensor: The augmented pose.
+        """
+        # param estimation
+        translation_fwd_mean = depth_map.median() / 2
+        translation_fwd_std = translation_fwd_mean * 0.1
+        translation_fwd = torch.normal(mean=translation_fwd_mean, std=translation_fwd_std)
+        
+        translated_pose = PoseSamplingBuilder.translate_pose(pose, torch.tensor([0.0, 0.0, translation_fwd], device=PoseSampler.DEVICE))
+        translated_pose = PoseSamplingBuilder.height_correction(translated_pose, pose) if auto_height_correction else translated_pose
+        
+        spherical_radius = depth_map.median() / 2
+        out_pose = PoseSamplingBuilder.spherical_pose_augmentation(translated_pose, spherical_radius, self.stddev)
+        return out_pose
+    
+    def uniform_driving(self, pose: torch.Tensor, depth_map: torch.Tensor, auto_height_correction: bool = True, **kwargs):
+        """
+        Performs translation forward followed by spherical augmentation. Estimates optimal parameters from depth map.
+        
+        Args:
+            pose (torch.Tensor): The pose to augment.
+            depth_map (torch.Tensor): The depth map from the pose.
+
+        Returns:
+            torch.Tensor: The augmented pose.
+        """
+        # param estimation
+        translation_fwd_max = depth_map.median() / 2
+        translation_fwd_min = - translation_fwd_max
+        translation_fwd = torch.rand(1, device=PoseSampler.DEVICE) * (translation_fwd_max - translation_fwd_min) + translation_fwd_min
+        
+        translated_pose = PoseSamplingBuilder.translate_pose(pose, torch.tensor([0.0, 0.0, translation_fwd], device=PoseSampler.DEVICE))
+        translated_pose = PoseSamplingBuilder.height_correction(translated_pose, pose) if auto_height_correction else translated_pose
+        
+        spherical_radius = depth_map.median() / 2
+        out_pose = PoseSamplingBuilder.spherical_pose_augmentation(translated_pose, spherical_radius, self.stddev)
+        return out_pose
+    
+    
+    def object_centric_spherical(self, pose: torch.Tensor, depth_map: torch.Tensor, **kwargs):
+        """
+        This is a go-to strategy for object-centric scenes. Performs only the spherical pose augmentation.
+        """
+        pose = pose.to(PoseSampler.DEVICE)
+        spherical_radius = depth_map.median() / 2
+        out_pose = PoseSamplingBuilder.spherical_pose_augmentation(pose, spherical_radius, self.stddev)
+        return out_pose
+    
+    def translate(self, pose):
+        """
+        Samples a new pose by translating along the circle with radius T_SIGMA * mean_depth / mean_focal_length.
+        """
+        pose = pose.to(PoseSampler.DEVICE)
+        pose = PoseSamplingBuilder.translate_pose(pose, torch.tensor([0.0, 0.0, 5.0], device=PoseSampler.DEVICE))
+        return pose
+
+    def sample(self, pose, depth_map: torch.Tensor = None, K: torch.Tensor = None):
+        """
+        This function provides interface for sampling a new pose. Internally, it calls the appropriate method
+        based on the strategy. Some arguments are used only for certain strategies.
+        """
+        if self.strategy == 'translation_only':
+            return self.ood_translation_only(pose, depth_map, K)
+        elif self.strategy == 'spherical_augmentation':
+            return self.ood_spherical_augmentation(pose)
+        elif self.strategy == 'spherical_auto':
+            return self.ood_spherical_auto(pose, depth_map)
+        elif self.strategy == 'object_centric_spherical':
+            return self.object_centric_spherical(pose, depth_map)
+        elif self.strategy == 'uniform_driving':
+            return self.uniform_driving(pose, depth_map)
+        else:
+            raise ValueError(f'Invalid strategy: {self.strategy}')
+
+
+
