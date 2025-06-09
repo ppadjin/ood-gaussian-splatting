75022dd20f8db8dc5807a3b1bd4fd6f00a5d7101
Pavle Pađin <43354887+ppadjin@users.noreply.github.com>
2025-06-09 15:30:00 +0100

    Update render_depth_mvc.py

--- render_depth_mvc.py
+++ render_depth_mvc.py
@@ -7,16 +7,16 @@ from oodgs.utils import read_input_data, get_git_root
 
 
 ood_loss = OODLoss()
-# Read input data from directory
+
 data_dir = os.path.join(get_git_root(), 'samples')
 
-# Load RGB images, depth maps, poses and calibrationx
+
 train_rgb, ood_train, \
     train_depth, ood_depth, \
     train_pose, ood_pose, \
     calibration_matrix = read_input_data(data_dir)
 
-# Compute depth multi-view consistency (MVC) score map
+
 score, score_map, oclusion_mask = ood_loss.forward_depth_mvc(
     gt_depth=train_depth,
     ood_depth=ood_depth,
@@ -26,7 +26,7 @@ score, score_map, oclusion_mask = ood_loss.forward_depth_mvc(
     use_oclusion_mask=True
 )
 
-# save score_map as heatmap
+
 score_map = score_map.cpu().numpy().squeeze()  # Remove singleton dimension (H,W,1) -> (H,W)
 plt.figure(figsize=(10, 8))
 plt.imshow(score_map, cmap='viridis')
